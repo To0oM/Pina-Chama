753376aac22e77e944a682343339265af8b35aaf
alonaya <alonayag@gmail.com>
2016-03-20 23:47:53 +0200

    add volunteersApp.js

---
+++ Pina-Chama/js/volunteersApp.js
@@ -0,0 +1,63 @@
+(function () {
+    var app = angular.module('volunteersApp', ['ngRoute']);
+    
+    app.controller('navContreoller', function(){
+        $("#navTab1").click(function(){
+            $(".activeTab").prop('class','navTabs');
+            $("#navTab1").prop('class','activeTab');
+        });
+        $("#navTab2").click(function(){
+            $(".activeTab").prop('class','navTabs');
+            $("#navTab2").prop('class','activeTab');
+        });
+        $("#navTab3").click(function(){
+            $(".activeTab").prop('class','navTabs');
+            $("#navTab3").prop('class','activeTab');
+        });
+        $("#navTab4").click(function(){
+            $(".activeTab").prop('class','navTabs');
+            $("#navTab4").prop('class','activeTab');
+        });
+    });
+    app.controller('mainController', function($scope) {
+        
+    });
+    app.controller('guidesController', function($scope) {
+        
+    });
+    app.controller('stockController', function($scope) {
+        
+    });
+    app.controller('arrangementController', function($scope) {
+        
+    });
+    
+    
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/arrangement', {
+            templateUrl: 'voluntaryArrangement.html',
+            controller: 'arrangementController',
+            controllerAs:'arrangement'
+        })
+            .when('/stock', {
+            templateUrl: 'outOfStock.html',
+            controller: 'stockController',
+            controllerAs: 'stock'
+        })
+            .when('/guides', {
+            templateUrl: 'guides.html',
+            controller: 'guidesController',
+            controllerAs: 'guides'
+        })
+            .when('/main', {
+            templateUrl: 'mainVolunteers.html',
+            controller: 'mainController',
+            controllerAs: 'main'
+        })
+            .otherwise({
+            redirectTo: '/main'
+        });
+
+    });
+})();
